b8707364596ec63892caf18785ce14400f890e04
Filipe Fernandes <filipe.fernandes@tqc.local>
2023-09-19 12:17:04 +0100

    bug fixed

--- src/components/Home/IngredientFilter.tsx
+++ src/components/Home/IngredientFilter.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import { Box, Stack, List, ListItem, ListItemText, IconButton } from "@mui/material";
 import { CustomText1, CustomIconButton1 } from "../../styling/GlobalStyles";
 import { Close, FilterAlt } from "@mui/icons-material";
-import { useGlobalContext } from "../../context/GlobalContext";
 import { useDispatch } from "react-redux";
 import { openIngredientFilterModal } from "../../features/modal/modalSlice";
 import { useSelector } from "react-redux";
 import { deleteItem } from "../../features/filters/IngredientFilterSlice";
 
 const IngredientFilter: React.FC = () => {
-  const { isExtraSmallScreen, isSmallScreen } = useGlobalContext();
   const dispatch = useDispatch();
   const { filteredIngredients } = useSelector((store: any) => store.ingredientFilter);
 
@@ -28,21 +26,20 @@ const IngredientFilter: React.FC = () => {
         </CustomIconButton1>
       </Stack>
       <List disablePadding>
-        {!(isExtraSmallScreen || isSmallScreen) &&
-          filteredIngredients.map((ingredient: string, index: number) => {
-            return (
-              <ListItem key={index} disablePadding>
-                <ListItemText>{ingredient}</ListItemText>
-                <IconButton
-                  edge="end"
-                  sx={{ padding: "0" }}
-                  onClick={() => dispatch(deleteItem(ingredient))}
-                >
-                  <Close />
-                </IconButton>
-              </ListItem>
-            );
-          })}
+        {filteredIngredients.map((ingredient: string, index: number) => {
+          return (
+            <ListItem key={index} disablePadding>
+              <ListItemText>{ingredient}</ListItemText>
+              <IconButton
+                edge="end"
+                sx={{ padding: "0" }}
+                onClick={() => dispatch(deleteItem(ingredient))}
+              >
+                <Close />
+              </IconButton>
+            </ListItem>
+          );
+        })}
       </List>
     </Box>
   );
